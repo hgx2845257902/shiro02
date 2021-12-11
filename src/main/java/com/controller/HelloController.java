f37febf652940ddd87a3b948f556b111ec9c0593
hgx2845257902 <2845257902@qq.com>
2021-12-11 14:01:01 +0800

    第二次

--- src/main/java/com/controller/HelloController.java
+++ src/main/java/com/controller/HelloController.java
@@ -4,8 +4,7 @@ public class HelloController {
 
     public static void main(String[] args) {
         System.out.println("haha");
-        System.out.println("oo");
-        System.out.println("uihghushugte");
+
     }
 
 
